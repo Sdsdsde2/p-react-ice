31b9639ff0570354f907a70d0646c8040a55cf3e
Sdsdsde2 <imspammails31@gmail.com>
2021-07-26 10:09:59 -0500

    added function component for rendering header and paragraph tags

---
+++ practice-app/src/components/TextComponents.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TextCompoents() {
+    return (
+        <div>
+            <h5>This is a header-5 tag</h5>
+            <h4>This is a header-4 tag</h4>
+            <h3>This is a header-3 tag</h3>
+            <h2>This is a header-2 tag</h2>
+            <h1>This is a header-1 tag</h1>
+            <p>This is a paragraph. It is for large bodies of text. The component that returns these text tags is called a "Functional Component".</p>
+        </div>
+    )
+}
+
+export default TextCompoents;
